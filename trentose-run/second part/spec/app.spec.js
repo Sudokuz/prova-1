be8432a3fc6eb5440da943b9323c5d12d56af525
Sergey <sergio_lebedev@yahoo.it>
2016-01-20 20:17:47 +0100

    jasmine test functions added

--- trentose-run/second part/spec/app.spec.js
+++ trentose-run/second part/spec/app.spec.js
@@ -1,7 +1,40 @@
 describe("App", function() {
+    describe("Coach.start", function() {
+  it("should not start with a negative number", function() { 
+      expect(Coach.start(-1)).toBe(false);
+  }); 
     
-  it("should..", function() { 
-      pending();
-  });  
- 
+  it("should start with a proper input", function() { 
+      expect(Coach.start("easy")).toBe(true);
+  });
+    });
+    
+  describe("Coach.track", function() {
+        
+  it("should not track with a wrong input", function() {
+      Coach.start("easy");
+      expect(Coach.track(0,10)).toBe(null);
+  });
+      
+  it("should track with a proper input", function() { 
+      expect(Coach.track("walk",10)).toBe(500);
+  });
+  
+  });
+  
+  describe("Coach.checkProgress", function() {
+        
+  it("should not check progress with a wrong input", function() { 
+      Coach.start(0);
+      expect(Coach.checkProgress()).toBe(null);
+  });
+      
+  it("should track with a proper input", function() { 
+      Coach.start("easy");
+      Coach.track("walk",10);
+      expect(Coach.checkProgress()).toBe(100);
+  });
+  
+  });
+    
 });
